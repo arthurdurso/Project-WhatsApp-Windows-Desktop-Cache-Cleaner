1468074f81868633146ab3da8a082845ada14fc0
Durso <dursoarthur@gmail.com>
2024-08-01 22:42:45 -0300

    Needed to remove directorys as well

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
+import shutil
 
 
-def remove_files_in_directory(directory_path):
+def remove_all_files_in_directory(directory_path):
     try:
         # List all files in the directory
         files = os.listdir(directory_path)
@@ -13,7 +14,8 @@ def remove_files_in_directory(directory_path):
                 os.remove(file_path)
                 print(f"Removed file: {file_path}")
             elif os.path.isdir(file_path):
-                print(f"Skipped directory: {file_path}")
+                shutil.rmtree(file_path)
+                print(f"Removed directory: {file_path}")
                 
     except Exception as e:
         print(f"Error: {e}")
@@ -21,4 +23,4 @@ def remove_files_in_directory(directory_path):
 
 # Usage
 directory_path = r'C:\Users\Durso\AppData\Local\Packages\5319275A.WhatsAppDesktop_cv1g1gvanyjgm\LocalState\shared\transfers'
-remove_files_in_directory(directory_path)
+remove_all_files_in_directory(directory_path)
